08285ea37a16e7ae983715374033222fbc758974
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-10 00:03:45 +0900

    initial commit

---
+++ MentorPreview/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MentorPreview
+//
+//  Created by ShinokiRyosei on 2017/10/10.
+//  Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - ViewController
+
+class ViewController: UIViewController {
+
+
+    // MARK: UIViewController
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+    }
+}
+
