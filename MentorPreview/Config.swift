9714d6ece202844b2117796a46d576aaf3621589
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-26 04:02:49 +0900

    Create Config

---
+++ MentorPreview/Config.swift
@@ -0,0 +1,18 @@
+//
+//  Config.swift
+//  MentorPreview
+//
+//  Created by ShinokiRyosei on 2017/10/26.
+//Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - Config
+
+class Config: NSObject {
+
+    static var baseURL: String = "https://mentor-preview.herokuapp.com/"
+
+}
