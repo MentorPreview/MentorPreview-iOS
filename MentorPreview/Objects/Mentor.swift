bd348bc3aca882e01670b5399037fd3be4144bfc
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-26 04:03:07 +0900

    Create Mentor

---
+++ MentorPreview/Objects/Mentor.swift
@@ -0,0 +1,16 @@
+//
+//  Mentor.swift
+//  MentorPreview
+//
+//  Created by ShinokiRyosei on 2017/10/26.
+//Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - Mentor
+
+class Mentor: NSObject {
+
+}
