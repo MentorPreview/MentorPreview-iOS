f206c0c241c0beb749c85c0e4b7f1ef0bb9a870d
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-27 02:03:26 +0900

    Create MentorsViewController

---
+++ MentorPreview/Scenes/Mentors/MentorsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MentorsViewController.swift
+//  MentorPreview
+//
+//  Created by ShinokiRyosei on 2017/10/27.
+//  Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - MentorsViewController
+
+class MentorsViewController: UIViewController, StoryboardInstantiable {
+
+
+    // MARK: Internal
+
+
+    // MARK: UIViewController
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+    }
+
+
+    // MARK: Private
+
+}
