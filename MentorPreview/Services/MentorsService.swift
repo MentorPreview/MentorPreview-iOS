5d5c336117ff16b0bfa5988cc65fc2392f948fef
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-27 01:48:45 +0900

    Create MentorsService

---
+++ MentorPreview/Services/MentorsService.swift
@@ -0,0 +1,16 @@
+//
+//  MentorsService.swift
+//  MentorPreview
+//
+//  Created by ShinokiRyosei on 2017/10/27.
+//  Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class MentorsService: NSObject {
+
+    func post() {
+        
+    }
+}
